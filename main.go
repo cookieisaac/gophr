372c92575f45f1a0f729cdee27591c7862bd0391
Ike Ma <ike@ca.ibm.com>
2016-07-12 14:43:00 -0400

    Version 2 and 3: Template and Layout

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 
 func main() {
 		mux := http.NewServeMux()
+		
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			RenderTemplate(w, r, "index/home", nil)
+		})
+		
 		mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
 		
 		log.Fatal(http.ListenAndServe(":3000", mux))
